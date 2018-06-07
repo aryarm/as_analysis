3c454ad01bb1390f179593b8102a2d673bd84a9c
Arya Massarat <aryam7@gmail.com>
2018-06-07 12:52:52 -0700

    add mkdir functionality to run-all

--- run-all.bash
+++ run-all.bash
@@ -6,5 +6,6 @@
 #$ -cwd
 
 out_path="out"
+mkdir -p $out_path
 
 snakemake -s Snakefile-variant_calling --cluster "qsub -t 1 -V -q iblm.q -j y -o ${out_path}/qout" -j 24 --config output_dir=${out_path} --latency-wait 10 >>${out_path}/out 2>&1
