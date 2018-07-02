61d2e1fc58f998906edcb1f645f09dac5fa5e05a
Arya Massarat <aryam7@gmail.com>
2018-07-02 16:08:39 -0700

    generalize remove_indels so it can be used with any counts file that starts with an rsID

--- scripts/remove_indels.r
+++ scripts/remove_indels.r
@@ -1,26 +1,27 @@
-# remove indels from a gzipped counts text file and print the result to standard out
+# remove indels from a (gzipped!) counts text file and print the result to standard out
 
 
 # get the input and output files from args
 args = commandArgs(trailingOnly = TRUE)
 counts = args[1]
+separator = args[2]
 
 # load the hets file from disk (note that it must be gzipped!)
-hets = read.table(gzfile(counts), sep=" ", header=F)
+hets = read.table(gzfile(counts), sep=separator, header=F)
 
 # name the cols appropriately
-colnames(hets) = c("chr", "start", "ref", "alt", "genotype", "ref.matches", "alt.matches", "errors")
+colnames(hets)[1:4] = c("chr", "start", "ref", "alt")
 
 # convert ref and alt cols to character vectors
 hets$ref = as.character(hets$ref)
 hets$alt = as.character(hets$alt)
 
-# subset
+# subset to keep snps only
 hets = hets[nchar(hets$ref) == 1,]
 hets = hets[nchar(hets$alt) == 1,]
 
 # write to stdout
-write.table(hets, file=stdout(), quote=FALSE, sep=" ", col.names = F, row.names=F)
+write.table(hets, file=stdout(), quote=FALSE, sep=separator, col.names = F, row.names=F)
 
 # if we got this far, we were probably successful
 quit(status=0)
